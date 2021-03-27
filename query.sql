77754adab0b79705962b6b3745139fca8b763ab3
VP <vijeta.puthran@gmail.com>
2021-03-27 16:37:40 -0500

    ipynb and sql filesupdated

---
+++ query.sql
@@ -0,0 +1,8 @@
+-- Query to check successful load
+select * from restaurant;
+
+select restaurant, 
+	   sales_in_millions, 
+	   city, 
+	   state 
+from restaurant;
---
+++ schema.sql
@@ -0,0 +1,8 @@
+-- Create Table
+CREATE TABLE restaurant (
+  index int	PRIMARY KEY,
+  restaurant char(100),
+  sales_in_millions float,
+  city char(40),
+  state char(40)	
+);
